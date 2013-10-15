6e98ca6f4b97c82b240e7cdddd970c452dae49a0
Debra Zapata <debra.zapata@gmail.com>
2013-10-15 12:34:25 -0600

    Initial commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,22 @@
+
+$(function(){
+	$("#signup-form").submit(function(e){
+		e.preventDefault()
+
+		//make a post request to our /signup endpoint
+		// serialize - takes form and looks through input and grabs value out of them
+		$.post('/formsubmit', $(this).serialize(), function(data){
+			console.log(data);
+
+			// if there was an erros, show the error
+			if(data.error){
+				$('#message').text(data.error)
+			}
+			
+			// if the request returned a success message, display it.
+			if(data.success){
+				$('#message').text(data.success)
+			}
+		})
+	});
+});
